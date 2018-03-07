4343ba42d97c48bfbcb53b0a1bbaf1b3817f6e56
Falah Shazib <falah.shazib@gmail.com>
2018-03-06 23:32:47 -0500

    added prop danceName

--- components/StatusItem/index.test.js
+++ components/StatusItem/index.test.js
@@ -3,21 +3,21 @@ import StatusItem from '..\\index';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={1209} status='Required internet connection'/>).toJSON();
+  const rendered = renderer.create(<StatusItem danceNum={1209} danceStatus='Required internet connection' danceName='the boogie'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={100} status='Uploaded'/>).toJSON();
+  const rendered = renderer.create(<StatusItem danceNum={100} danceStatus='Uploaded' danceName='121'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={1209} status='9'/>).toJSON();
+  const rendered = renderer.create(<StatusItem danceNum={1209} danceStatus='9' danceName='waveeeeee'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
 
 it('renders without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={String} status='Uploaded'/>).toJSON();
+  const rendered = renderer.create(<StatusItem danceNum={String} danceStatus='Uploaded' danceName='whoa'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
