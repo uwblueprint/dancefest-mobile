4a00e68e05c642213b2c1db747e592169e55141f
George Ke <georgekeee@gmail.com>
2018-03-07 00:02:56 -0500

    merged buttons

--- components/RadioButtons/index.js
+++ components/RadioButtons/index.js
@@ -10,16 +10,31 @@ const RadioButtons = ({ buttonNames, input: { onChange, value } }) => {
   const chosenIndex = buttonNames.indexOf(value);
 
   return (
-    <View style={style.buttonCollection}>
-      {buttonNames.map((button, i) => (
-        <TouchableHighlight
-          key={i}
-          style={[style.button, chosenIndex === i ? style.selectedButton : {}]}
-          onPress={() => onChange(button)}
-          underlayColor="#fff">
-          <Text style={chosenIndex === i ? style.selectedText : {}}>{button}</Text>
-        </TouchableHighlight>
-      ))}
+    <View style={mergeButtons ? style.mergedButtonCollection: style.buttonCollection}>
+      {buttonNames.map((button, i) => {
+        var styleArray = [];
+        if (mergeButtons) {
+          styleArray.push(style.mergedButton);
+          if (i == 0) {
+            styleArray.push(style.leftMergedButton);
+          } else if (i == buttonNames.length - 1) {
+            styleArray.push(style.rightMergedButton);
+          }
+        } else {
+          styleArray.push(style.button);
+        }
+        styleArray.push(chosenIndex === i ? style.selectedButton : {});
+
+        return (
+          <TouchableHighlight
+            key={i}
+            style={styleArray}
+            onPress={() => onChange(button)}
+            underlayColor='#fff'>
+              <Text style={[style.text, chosenIndex === i ? style.selectedText : {}]}>{button}</Text>
+          </TouchableHighlight>
+        )
+      })}
     </View>
   );
 };
@@ -30,6 +45,7 @@ RadioButtons.propTypes = {
     onChange: PropTypes.func.isRequired,
     value: PropTypes.string.isRequired,
   }).isRequired,
+  mergeButtons: PropTypes.bool,
 };
 
 /**
@@ -41,6 +57,11 @@ const style = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
   },
+  mergedButtonCollection: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+  },
   button: {
     borderRadius: 5,
     borderColor: '#BBBBBB',
@@ -48,9 +69,31 @@ const style = StyleSheet.create({
     margin: 5,
     padding: 10,
   },
+  mergedButton: {
+    borderColor: '#BBBBBB',
+    borderBottomWidth: 1,
+    borderLeftWidth: 0.5,
+    borderRightWidth: 0.5,
+    borderTopWidth: 1,
+    flex: 1,
+    padding: 10,
+  },
+  leftMergedButton: {
+    borderTopLeftRadius: 9,
+    borderBottomLeftRadius: 9,
+    borderLeftWidth: 1,
+  },
+  rightMergedButton: {
+    borderTopRightRadius: 9,
+    borderBottomRightRadius: 9,
+    borderRightWidth: 1,
+  },
   selectedButton: {
     borderColor: '#EB6284',
     backgroundColor: '#EB6284',
+  },
+  text: {
+    textAlign: 'center',
   },
   selectedText: {
     color: '#fff',
