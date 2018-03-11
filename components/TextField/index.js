4ede210fa1c89e425a89434d35465df7ffc3c372
katrinacrisostomo <katrina@watsi.org>
2018-03-11 16:30:16 -0400

    Eslint fixes: fix textfield eslint errors
    [#26]

--- components/TextField/index.js
+++ components/TextField/index.js
@@ -1,35 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, View, TextInput } from 'react-native';
 
 /**
  * Component
  */
 
-const TextField = ({ numberOfLines, input: { onChange, value } }) => {
-  return (
-    <View style={styles.textField}>
-      <TextInput multiline={numberOfLines > 1}
-                 numberOfLines={numberOfLines}
-                 onChangeText={(text) => onChange(text)}
-                 style={[styles.inputField, numberOfLines > 1 ? styles.largeInputField : {}]}
-                 underlineColorAndroid={'transparent'}
-                 value={value} />
-    </View>
-  );
-}
+const TextField = ({ numberOfLines, input: { onChange, value } }) => (
+  <View style={styles.textField}>
+    <TextInput
+      multiline={numberOfLines > 1}
+      numberOfLines={numberOfLines}
+      onChangeText={text => onChange(text)}
+      style={[styles.inputField, numberOfLines > 1 ? styles.largeInputField : {}]}
+      underlineColorAndroid="transparent"
+      value={value} />
+  </View>
+);
 
 TextField.propTypes = {
   numberOfLines: PropTypes.number,
   input: PropTypes.shape({
     onChange: PropTypes.func.isRequired,
     value: PropTypes.string.isRequired,
-  }),
-}
+  }).isRequired,
+};
 
 TextField.defaultProps = {
   numberOfLines: 1,
-}
+};
 
 const styles = StyleSheet.create({
   textField: {
@@ -47,7 +46,7 @@ const styles = StyleSheet.create({
   largeInputField: {
     textAlignVertical: 'top',
     paddingVertical: 10,
-  }
+  },
 });
 
 /**
