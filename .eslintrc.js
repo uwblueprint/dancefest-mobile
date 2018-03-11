b7a1fcf4f0d92e3a329a415f74123d82f10654ae
katrinacrisostomo <katrina@watsi.org>
2018-03-11 14:36:07 -0400

    Eslint settings: change jsx-closing-bracket-location to be after-props to reduce unnecessary lines for closing tags
    [#26]

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,9 @@ module.exports = {
       "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
       "no-use-before-define": ["error", { "variables": false }],
       "react/no-array-index-key": ["warn"],
+      "react/jsx-closing-bracket-location": ["error", {
+        "nonEmpty": "after-props" || false,
+        "selfClosing": "after-props" || false
+      }],
     }
 };
