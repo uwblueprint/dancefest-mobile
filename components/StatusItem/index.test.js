236d88229402e09d35bc5215af4463d3488d0c5b
Falah Shazib <falah.shazib@gmail.com>
2018-03-11 19:14:16 -0400

    improved test description

--- components/StatusItem/index.test.js
+++ components/StatusItem/index.test.js
@@ -2,22 +2,17 @@ import React from 'react';
 import StatusItem from '..\\index';
 import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
+it('Renders exclamation without crashing', () => {
   const rendered = renderer.create(<StatusItem danceNum={1209} danceStatus='Required internet connection' danceName='the boogie'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={100} danceStatus='Uploaded' danceName='121'/>).toJSON();
+it('Renders checkmark without crashing', () => {
+  const rendered = renderer.create(<StatusItem danceNum={101} danceStatus='Uploaded' danceName='string'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
 
-it('renders without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={1209} danceStatus='9' danceName='waveeeeee'/>).toJSON();
-  expect(rendered).toBeTruthy();
-});
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={String} danceStatus='Uploaded' danceName='whoa'/>).toJSON();
+it('Renders loop without crashing', () => {
+  const rendered = renderer.create(<StatusItem danceNum={1} danceStatus='Loading' danceName='fya'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
