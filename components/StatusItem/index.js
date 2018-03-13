cf48a100969190f5911a86602c1597cb8abf2edf
Falah Shazib <falah.shazib@gmail.com>
2018-03-12 22:59:35 -0400

    lint edits

--- components/StatusItem/index.js
+++ components/StatusItem/index.js
@@ -1,64 +1,55 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet, Image } from 'react-native';
-import { Constants } from 'expo';
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 import PropTypes from 'prop-types';
 import Icon from './../Icon';
 
-
-class StatusItemIcon extends Component {
-  static propTypes = {
-    danceStatus: PropTypes.string.isRequired,
-  }
-
-  render() {
-    if (this.props.danceStatus === 'Uploaded') {
-      return (
-        <Icon name="Checkmark" height="50" width="50" fill='grey' viewBox='0 0 30 30' />
-      );
-    } else if (this.props.danceStatus === 'Required internet connection') {
-      return (
-        <Icon name="Exclamation" height="50" width="50" fill='red' viewBox='0 0 30 30' />
-      );
-    } else if (this.props.danceStatus === 'Loading') {
-      return (
-        <Icon name = "Loop" height="50" width="50" fill='black' viewBox='0 0 30 30' />
-      );
-    }
-  }
-}
-
-class Information extends Component {
-  static propTypes = {
-    danceNum: PropTypes.number.isRequired,
-    danceName: PropTypes.string.isRequired,
-  }
-
-  render() {
+const StatusItemIcon = ({ danceStatus }) => {
+  if (danceStatus === 'Uploaded') {
     return (
-      <View style={style.information}>
-        <Text style={style.danceNum}>#{this.props.danceNum}</Text>
-        <Text style={style.danceName}>{this.props.danceName}</Text>
-      </View>
+      <Icon name="Checkmark" height="50" width="50" fill="grey" viewBox="0 0 30 30" />
+    );
+  } else if (danceStatus === 'Loading') {
+    return (
+      <Icon name="Loop" height="50" width="50" fill="black" viewBox="0 0 30 30" />
+    );
+  } else if (danceStatus === 'Required internet connection') {
+    return (
+      <Icon name="Exclamation" height="50" width="50" fill="red" viewBox="0 0 30 30" />
     );
   }
-}
+  return (null);
+};
 
-export default class StatusItem extends React.Component {
-  static propTypes = {
-    danceNum: PropTypes.number.isRequired,
-    danceStatus: PropTypes.string.isRequired,
-    danceName: PropTypes.string.isRequired,
-  }
+StatusItemIcon.propTypes = {
+  danceStatus: PropTypes.string.isRequired,
+};
 
-  render () {
-    return (
-      <View style={style.items}>
-        <Information danceNum={this.props.danceNum} danceName={this.props.danceName} />
-        <StatusItemIcon danceStatus={this.props.danceStatus} />
-      </View>
-    );
-  }
-}
+const Information = ({ danceNum, danceName }) => (
+  <View style={style.information}>
+    <Text style={style.danceNum}>#{danceNum}</Text>
+    <Text style={style.danceName}>{danceName}</Text>
+  </View>
+);
+
+
+Information.propTypes = {
+  danceNum: PropTypes.number.isRequired,
+  danceName: PropTypes.string.isRequired,
+};
+
+const StatusItem = ({ danceNum, danceName, danceStatus }) => (
+  <View style={style.items}>
+    <Information danceNum={danceNum} danceName={danceName} />
+    <StatusItemIcon danceStatus={danceStatus} />
+  </View>
+);
+
+
+StatusItem.propTypes = {
+  danceNum: PropTypes.number.isRequired,
+  danceStatus: PropTypes.string.isRequired,
+  danceName: PropTypes.string.isRequired,
+};
 
 const style = StyleSheet.create({
   items: {
@@ -70,7 +61,7 @@ const style = StyleSheet.create({
     borderRadius: 4,
     borderWidth: 0.5,
     borderColor: 'black',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
   danceNum: {
     alignItems: 'flex-start',
@@ -93,3 +84,5 @@ const style = StyleSheet.create({
     height: 'auto',
   },
 });
+
+export default StatusItem;
