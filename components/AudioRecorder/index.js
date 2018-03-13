50d7df381170d676a46c6e31ef1e0013f9d83e33
George Ke <georgekeee@gmail.com>
2018-03-13 16:36:29 -0400

    red button starts/pauses recording + make duration update

--- components/AudioRecorder/index.js
+++ components/AudioRecorder/index.js
@@ -11,31 +11,18 @@ export default class AudioRecorder extends React.Component {
     super(props);
     this.state = {
       isRecording: false,
-      recordingDuration: 100000000,
+      recordingDuration: 0,
       recording: null,
     };
   }
 
-  async redo() {
-    if (this.state.isRecording) {
-      await this.stopRecording();
-    }
+  updateDuration(status) {
     this.setState({
-      isRecording: false,
-      recordingDuration: 0,
-      recording: null,
+      recordingDuration: status.durationMillis || 0,
     });
   }
 
   async toggleRecording() {
-    if (this.state.isRecording) {
-      await this.stopRecording();
-    } else {
-      await this.startRecording();
-    }
-  }
-
-  async startRecording() {
     let newRecording = null;
 
     try {
@@ -54,10 +41,15 @@ export default class AudioRecorder extends React.Component {
         });
 
         // audio file extension will be .m4a for iOS and Android
-        const recordingOptions = Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY;
-        recordingOptions.ios.extension = '.m4a';
-        recordingOptions.ios.outputFormat = Audio.RECORDING_OPTION_IOS_OUTPUT_FORMAT_APPLELOSSLESS;
+        const recordingOptions = _.merge(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY, {
+          ios: {
+            extension: '.m4a',
+            outputFormat: Audio.RECORDING_OPTION_IOS_OUTPUT_FORMAT_APPLELOSSLESS,
+          },
+        });
 
+        newRecording.setOnRecordingStatusUpdate(status => this.updateDuration(status));
+        newRecording.setProgressUpdateInterval(50);
         await newRecording.prepareToRecordAsync(recordingOptions);
         recording = newRecording;
       } else {
@@ -78,7 +70,7 @@ export default class AudioRecorder extends React.Component {
     // only update state if everything ran successfully
     const stateUpdate = {
       isRecording: !this.state.isRecording,
-    }
+    };
     if (newRecording) {
       stateUpdate.recording = newRecording;
     }
@@ -100,6 +92,7 @@ export default class AudioRecorder extends React.Component {
 
       this.setState({
         isRecording: false,
+        recordingDuration: 0,
         recording: null,
       });
     }
@@ -109,7 +102,7 @@ export default class AudioRecorder extends React.Component {
     const { recordingDuration } = this.state;
     const hours = _.padStart(Math.floor(recordingDuration / 3600000), 2, '0');
     const minutes = _.padStart(Math.floor((recordingDuration % 3600000) / 60000), 2, '0');
-    const seconds = _.padStart((recordingDuration % 60000) / 1000, 2, '0');
+    const seconds = _.padStart(Math.ceil((recordingDuration % 60000) / 1000), 2, '0');
     return (
       <View style={styles.audioRecorder}>
         <Image source={micIcon} />
@@ -117,12 +110,12 @@ export default class AudioRecorder extends React.Component {
         <Text style={styles.subText}>{this.state.isRecording ? 'Recording' : ' '}</Text>
         <View style={styles.buttonContainer}>
           <TouchableHighlight
-            onPress={async () => await this.redo()}
+            onPress={async () => this.stopRecording()}
             style={styles.redoButton}>
             <Image source={redoIcon} />
           </TouchableHighlight>
           <TouchableHighlight
-            onPress={async () => await this.toggleRecording()}
+            onPress={async () => this.toggleRecording()}
             style={styles.recordButtonOutline}>
             <View style={this.state.isRecording ? styles.stopButton : styles.recordButton} />
           </TouchableHighlight>
