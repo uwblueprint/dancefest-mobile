9a2449513a92184e06226692e822c6b557f6a002
Falah Shazib <falah.shazib@gmail.com>
2018-03-13 22:52:29 -0400

    added DANCE_STATUS Constant

--- components/StatusItem/index.test.js
+++ components/StatusItem/index.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import StatusItem from './index';
+import { StatusItem, DANCE_STATUS } from './index';
 import renderer from 'react-test-renderer';
 
 it('Renders exclamation without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={1209} danceStatus='Required internet connection' danceName='the boogie'/>).toJSON();
+  const rendered = renderer.create(<StatusItem danceNum={1209} danceStatus={DANCE_STATUS.requireInternet} danceName='the boogie'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
 
 it('Renders checkmark without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={101} danceStatus='Uploaded' danceName='string'/>).toJSON();
+  const rendered = renderer.create(<StatusItem danceNum={101} danceStatus={DANCE_STATUS.uploaded} danceName='string'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
 
 it('Renders loop without crashing', () => {
-  const rendered = renderer.create(<StatusItem danceNum={1} danceStatus='Loading' danceName='fya'/>).toJSON();
+  const rendered = renderer.create(<StatusItem danceNum={1} danceStatus={DANCE_STATUS.loading} danceName='fya'/>).toJSON();
   expect(rendered).toBeTruthy();
 });
