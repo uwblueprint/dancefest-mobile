bc16dcff3f41ca3e14deef9b4bb980d852913dfc
katrinacrisostomo <katrina@watsi.org>
2018-03-13 23:48:08 -0400

    Add marks as props to DanceCritiqueForm
    [#39]

--- components/DanceCritiqueForm/index.js
+++ components/DanceCritiqueForm/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { reduxForm, Field } from 'redux-form';
 import { StyleSheet, Text, View } from 'react-native';
 import RadioButtons from '../RadioButtons';
 
 class DanceCritiqueForm extends React.Component {
+  static propTypes = {
+    danceNumber: PropTypes.number.isRequired,
+    techniqueMark: PropTypes.number.isRequired,
+    spatialAwarenessMark: PropTypes.number.isRequired,
+    useOfMusicTextSilenceMark: PropTypes.number.isRequired,
+    communicationElementsMark: PropTypes.number.isRequired,
+    communicationMark: PropTypes.number.isRequired,
+  }
+
   render() {
     return (
       <View style={styles.container}>
