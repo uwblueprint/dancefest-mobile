c34dcd5f2090ed24f21ec147cd84854d3285a8b4
sheethala <s2swamin@uwaterloo.ca>
2018-03-19 19:14:24 -0400

    Added back rootreducer

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 
 import DanceCritiqueForm from './components/DanceCritiqueForm'
+import rootReducer from './rootReducer.js'
 
 const store = createStore(rootReducer)
 
