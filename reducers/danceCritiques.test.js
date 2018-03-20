b2ef366bfdd2aac911e62b7de6d12b08b3a1298c
katrinacrisostomo <katrina@watsi.org>
2018-03-19 23:34:04 -0400

    Update tests to reflect changes
    [#54]

--- reducers/danceCritiques.test.js
+++ reducers/danceCritiques.test.js
@@ -1,6 +1,5 @@
 import danceCritiques, {
   initialState,
-  UPLOAD_DANCE_CRITIQUE_REQUEST,
   UPLOAD_DANCE_CRITIQUE_SUCCESS,
   UPLOAD_DANCE_CRITIQUE_FAILURE,
 } from './danceCritiques';
@@ -8,17 +7,6 @@ import danceCritiques, {
 describe('danceCritiques', () => {
   it('returns the initial state', () => {
     expect(danceCritiques(undefined, {})).toEqual(initialState());
-  });
-
-  it('handles UPLOAD_DANCE_CRITIQUE_REQUEST', () => {
-    const action = { type: UPLOAD_DANCE_CRITIQUE_REQUEST };
-
-    const expected = expect.objectContaining({
-      isUploadingDanceCritique: true,
-      uploadDanceCritiqueError: '',
-    });
-
-    expect(danceCritiques(undefined, action)).toEqual(expected);
   });
 
   it('handles UPLOAD_DANCE_CRITIQUE_SUCCESS', () => {
@@ -31,7 +19,6 @@ describe('danceCritiques', () => {
     };
 
     const expected = expect.objectContaining({
-      isUploadingDanceCritique: false,
       uploadDanceCritiqueError: '',
       notUploadedDanceCritiques: [4, 5],
       uploadedDanceCritiques: [1, 2, 3],
@@ -41,11 +28,16 @@ describe('danceCritiques', () => {
   });
 
   it('handles UPLOAD_DANCE_CRITIQUE_FAILURE', () => {
-    const action = { type: UPLOAD_DANCE_CRITIQUE_FAILURE, errorMessage: 'Something went wrong.' };
+    const action = {
+      type: UPLOAD_DANCE_CRITIQUE_FAILURE,
+      googleDriveErrorMessage: 'Something went wrong with uploading the audio.',
+      googleSheetsErrorMessage: 'Something went wrong with uploading the critique.',
+    };
 
     const expected = expect.objectContaining({
       isUploadingDanceCritique: false,
-      uploadDanceCritiqueError: 'Something went wrong.',
+      googleDriveErrorMessage: 'Something went wrong with uploading the audio.',
+      googleSheetsErrorMessage: 'Something went wrong with uploading the critique',
     });
   });
 });
