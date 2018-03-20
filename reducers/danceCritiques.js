663010cccb17e8810ec91e4ab1f5ab0227e997a7
katrinacrisostomo <katrina@watsi.org>
2018-03-19 22:07:47 -0400

    Add reducer for dance critiques
    [#54]

---
+++ reducers/danceCritiques.js
@@ -0,0 +1,78 @@
+/**
+ * Helpers
+ */
+
+const deleteItemAtIndex = index => arr => [...arr.slice(0, index), ...arr.slice(index + 1)];
+
+/**
+ * Initial state
+ */
+
+export function initialState () {
+  return {
+    // TODO: these initial arrays should populate with values from past instance of app
+    uploadedDanceCritiques: [],
+    notUplodaedDanceCritiques: [],
+    isUploadingDanceCritique: false,
+    uploadDanceCritiqueError: '',
+  };
+}
+
+/**
+ * Actions
+ */
+
+export const UPLOAD_DANCE_CRITIQUE_REQUEST = 'UPLOAD_DANCE_CRITIQUE_REQUEST';
+export const UPLOAD_DANCE_CRITIQUE_SUCCESS = 'UPLOAD_DANCE_CRITIQUE_SUCCESS';
+export const UPLOAD_DANCE_CRITIQUE_FAILURE = 'UPLOAD_DANCE_CRITIQUE_FAILURE';
+
+export function uploadDanceCritique(danceCritique) {
+  // TODO: send to Google Sheet here
+  // TODO: send to Google Drive here
+
+  const danceId = danceCritique.danceId;
+
+  return {
+    types: [UPLOAD_DANCE_CRITIQUE_REQUEST, UPLOAD_DANCE_CRITIQUE_SUCCESS, UPLOAD_DANCE_CRITIQUE_FAILURE],
+    errorMessage: 'Something went wrong, please try again.',
+    danceId,
+  };
+}
+
+/**
+ * Reducer
+ */
+
+export default function danceCritiques (state = initialState(), action = {}) {
+  switch (action.type) {
+    case UPLOAD_DANCE_CRITIQUE_REQUEST:
+      return {
+        ...state,
+        isUploadingDanceCritique: true,
+        uploadDanceCritiqueError: '',
+        notUploadedDanceCritiques: state.notUploadedDanceCritiques,
+        uploadedDanceCritiques: state.uploadedDanceCritiques,
+      }
+    case UPLOAD_DANCE_CRITIQUE_SUCCESS:
+      const danceId = action.danceId;
+      const index = (state.notUploadedDanceCritiques).indexOf(danceId);
+
+      return {
+        ...state,
+        isUploadingDanceCritique: false,
+        uploadDanceCritiqueError: '',
+        notUploadedDanceCritiques: deleteItemAtIndex(index)(state.notUploadedDanceCritiques),
+        uploadedDanceCritiques: state.uploadedDanceCritiques.concat(danceId);
+      }
+    case UPLOAD_DANCE_CRITIQUE_FAILURE:
+      return {
+        ...state,
+        isUploadingDanceCritique: false,
+        uploadDanceCritiqueError: action.errorMessage,
+        notUploadedDanceCritiques: state.notUploadedDanceCritiques,
+        uploadedDanceCritiques: state.uploadedDanceCritiques,
+      }
+    default:
+      return state;
+  }
+}
