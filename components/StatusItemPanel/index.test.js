86b6cdea134a85bbc25e987f4e5c63e740c2e3c4
Falah Shazib <falah.shazib@gmail.com>
2018-03-19 22:35:51 -0400

    added tests

---
+++ components/StatusItemPanel/index.test.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { DANCE_STATUS }  from './../StatusItem/index';
+import StatusItemPanel from './index';
+import renderer from 'react-test-renderer';
+
+it('Renders empty list without crashing', () => {
+  const rendered = renderer.create(<StatusItemPanel />).toJSON();
+  expect(rendered).toBeTruthy();
+})
+
+it('Renders exclamation without crashing', () => {
+  const rendered = renderer.create(<StatusItemPanel statusItemData={[ { danceNum:103, danceStatus:DANCE_STATUS.requireInternet, danceName:'PrintTheBlue' }, ]}/>).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('Renders checkmark without crashing', () => {
+  const rendered = renderer.create(<StatusItemPanel statusItemData={[ { danceNum:101, danceStatus:DANCE_STATUS.uploaded, danceName:'Plz review me' }, ]}/>).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('Renders loop without crashing', () => {
+  const rendered = renderer.create(<StatusItemPanel statusItemData={[ { danceNum:107, danceStatus:DANCE_STATUS.loading, danceName:'DancefestDab' }, ]}/>).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('Renders mixed list without crashing', () => {
+  const rendered = renderer.create(<StatusItemPanel statusItemData={[
+                                  { danceNum:103, danceStatus:DANCE_STATUS.requireInternet, danceName:'PrintTheBlue' },
+                                  { danceNum:107, danceStatus:DANCE_STATUS.loading, danceName:'DancefestDab' },
+                                  { danceNum:1, danceStatus:DANCE_STATUS.requireInternet, danceName:'The insanely super super long name that goes on forever' },
+                                  { danceNum:101, danceStatus:DANCE_STATUS.uploaded, danceName:'Plz review me' },
+                                  { danceNum:1209, danceStatus:DANCE_STATUS.loading, danceName:'the boogie'},
+                  ]}/>).toJSON();
+  expect(rendered).toBeTruthy();
+});
+
+it('Renders mixed list longer than screen without crashing', () => {
+  const rendered = renderer.create(<StatusItemPanel statusItemData={[
+                                  { danceNum:103, danceStatus:DANCE_STATUS.requireInternet, danceName:'PrintTheBlue' },
+                                  { danceNum:107, danceStatus:DANCE_STATUS.loading, danceName:'DancefestDab' },
+                                  { danceNum:1, danceStatus:DANCE_STATUS.requireInternet, danceName:'The insanely super super long name that goes on forever' },
+                                  { danceNum:101, danceStatus:DANCE_STATUS.uploaded, danceName:'Plz review me' },
+                                  { danceNum:1209, danceStatus:DANCE_STATUS.loading, danceName:'the boogie'},
+                                  { danceNum:1103, danceStatus:DANCE_STATUS.requireInternet, danceName:'PrintTheBlue' },
+                                  { danceNum:1107, danceStatus:DANCE_STATUS.loading, danceName:'DancefestDab' },
+                                  { danceNum:11, danceStatus:DANCE_STATUS.requireInternet, danceName:'The insanely super super long name that goes on forever' },
+                                  { danceNum:1101, danceStatus:DANCE_STATUS.uploaded, danceName:'Plz review me' },
+                                  { danceNum:11209, danceStatus:DANCE_STATUS.loading, danceName:'the boogie'},
+                                  { danceNum:203, danceStatus:DANCE_STATUS.requireInternet, danceName:'PrintTheBlue' },
+                                  { danceNum:207, danceStatus:DANCE_STATUS.loading, danceName:'DancefestDab' },
+                                  { danceNum:2, danceStatus:DANCE_STATUS.requireInternet, danceName:'The insanely super super long name that goes on forever' },
+                                  { danceNum:201, danceStatus:DANCE_STATUS.uploaded, danceName:'Plz review me' },
+                                  { danceNum:2209, danceStatus:DANCE_STATUS.loading, danceName:'the boogie'},
+                  ]}/>).toJSON();
+  expect(rendered).toBeTruthy();
+});
