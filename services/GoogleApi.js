958dcb170c7af1f9813639f6d8e2cd61ebfd0f0b
George Ke <georgekeee@gmail.com>
2018-03-21 05:56:33 -0400

    use shared dancefest blueprint email to generate ids

--- services/GoogleApi.js
+++ services/GoogleApi.js
@@ -1,9 +1,9 @@
 export async function signInWithGoogleAsync() {
   try {
     const result = await Expo.Google.logInAsync({
-      androidClientId: '939408090084-goeojtkur4qj31f1iosrdf2ker47vfkp.apps.googleusercontent.com',
-      iosClientId: '939408090084-022pmge4h770dj1dk4u0i64fpk0bqqjm.apps.googleusercontent.com',
-      scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+      androidClientId: '620228359926-gr8206oqn5ci5hdauao697qmpb8inj08.apps.googleusercontent.com',
+      iosClientId: '620228359926-l9um9agdpoogfs82thfi1u5g7pjsgniu.apps.googleusercontent.com',
+      scopes: ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file'],
     });
 
     if (result.type === 'success') {
