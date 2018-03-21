bf259add0579769ebefa46897bd0b6de67e82a71
Sheethala <s2swamin@uwaterloo.ca>
2018-03-21 00:48:01 -0400

    Create navigation (#56)
    * Created basic navigation
    
    * Working commit of buttons
    
    * Removed navigator
    
    * Updated Package.json
    
    Removed react-navigation
    
    * Fixed whitespace
    
    * Added back rootreducer
    
    * Added more screens to danceCritiqueForm
    
    * More CSS changes, added dance styles
    
    * Changed names to match the ones in the store
    
    * Changes to the reducers and more CSS changes
    
    * Removed scrollview from imports

--- components/TextField/index.js
+++ components/TextField/index.js
@@ -35,17 +35,19 @@ const styles = StyleSheet.create({
     alignSelf: 'stretch',
   },
   inputField: {
-    backgroundColor: 'white',
+    backgroundColor: 'black',
     borderWidth: 1,
     borderRadius: 5,
     borderStyle: 'dashed',
     borderColor: 'lightgrey',
-    paddingHorizontal: 10,
-    paddingVertical: 5,
+    color: 'white',
+    //paddingHorizontal: 10,
+    //paddingVertical: 5,
+    fontSize: 30,
   },
   largeInputField: {
     textAlignVertical: 'top',
-    paddingVertical: 10,
+    //paddingVertical: 10,
   },
 });
 
