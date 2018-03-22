51e606e7e4ca47f2caf8414b66bd24d5665f7f1b
jtaaa <joshuatallum@gmail.com>
2018-03-21 23:53:13 -0400

    Add audio uploading service

---
+++ services/UploadRecording.js
@@ -0,0 +1,25 @@
+export default async function uploadAudioAsync(uri) {
+    console.log("Uploading " + uri);
+    let apiUrl = 'https://uwblueprint-dancefest.herokuapp.com/upload';
+    let uriParts = uri.split('.');
+    let fileType = uriParts[uriParts.length - 1];
+  
+    let formData = new FormData();
+    formData.append('file', {
+      uri,
+      name: `recording.${fileType}`,
+      type: `audio/x-${fileType}`,
+    });
+  
+    let options = {
+      method: 'POST',
+      body: formData,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'multipart/form-data',
+      },
+    };
+  
+    console.log("POSTing " + uri + " to " + apiUrl);
+    return fetch(apiUrl, options);
+  }
