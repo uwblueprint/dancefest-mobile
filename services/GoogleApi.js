5fbfd00c7ae1bd7b3edac11fc24b408f1dc7a812
Samudraneel <s24bhatt@edu.uwaterloo.ca>
2018-06-05 22:45:54 -0400

    Made a slight non-issue modification

--- services/GoogleApi.js
+++ services/GoogleApi.js
@@ -7,12 +7,11 @@ const config =
 } :{
    androidStandaloneAppClientId: '620228359926-6fc09eiven81esan1uh3eg003fa812hs.apps.googleusercontent.com',
    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-}:
-{
-  androidClientId: '620228359926-gr8206oqn5ci5hdauao697qmpb8inj08.apps.googleusercontent.com',
-  iosClientId: '620228359926-l9um9agdpoogfs82thfi1u5g7pjsgniu.apps.googleusercontent.com',
-  webClientId: '620228359926-hu7e9luilvk67fa3qvper6dnkep68jj6.apps.googleusercontent.com',
-  scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+} :{
+   androidClientId: '620228359926-gr8206oqn5ci5hdauao697qmpb8inj08.apps.googleusercontent.com',
+   iosClientId: '620228359926-l9um9agdpoogfs82thfi1u5g7pjsgniu.apps.googleusercontent.com',
+   webClientId: '620228359926-hu7e9luilvk67fa3qvper6dnkep68jj6.apps.googleusercontent.com',
+   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 };
 
 export async function signInWithGoogleAsync() {
